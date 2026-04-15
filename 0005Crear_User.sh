fcae304ac1dcdc22a1b2c7c9a0ae25f9438b7af6
MiquelR <mrodriguezmacias@esliceu.net>
2026-04-15 16:17:02 +0200

    errada usuari duplicat

--- 0005Crear_User.sh
+++ 0005Crear_User.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
-function Usage()(
-    echo "Usage ${0}"
+function usage(){
+    echo "usage ${0}"
     echo
     echo "Has de ser root"
+    echo "L'usuari no és pot repetir"
     exit 1 #Indica una errada
-)
+}
+
 #Script per crear un usuari
+
 #Primer comprovam si som root
+
 if [[ ${UID} -ne 0 ]]
 then
-    Usage
+    usage
 fi
+
 read -p "Introdueix el nom complet: " COMMENTS
+
 #Demanar el nom d'usuari
 read -p "Introdueix el nom d'usuari: " USER_NAME
+
 #Demanar el password
 read -p "Introdueix el password: " PASSWORD
+
 #Creariam l'usuari
-usseradd -m -c "${COMMENTS}" ${USER_NAME} &> /dev/null
-echo "Creant l'usuari ${USER_NAME}"
+useradd -m -c "${COMMENTS}" "${USER_NAME}"
+
 #Control darrera comanda
-if [[ ${?} -ne 0 ]]
+if [[ $? -ne 0 ]]
 then
     echo "Errada creant l'usuari"
     exit 1
 fi
+
+echo "Creant l'usuari ${USER_NAME}"
+
+
+
 
 exit 0
